00bf73b9c15a86cc4b69f40156cb863b3295da4a
Kathryn <kathryn.chu10@gmail.com>
2019-03-13 22:16:56 -0700

    Successfully set up d3 and webpack; compilation successful

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("working");
+import * as d3 from 'd3';
+
+console.log("why are these squares not turning orange");
+
+const square = d3.selectAll("rect");
+square.style("fill", "orange");
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-//   template: './src/index.html',
-//   filename: 'index.html',
-//   inject: 'body'
-// });
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './dist/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  }, 
+  module: { 
+    rules: [ 
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  }, 
+  plugins: [HtmlWebpackPluginConfig]
 };
