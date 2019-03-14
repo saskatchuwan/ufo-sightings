a77695c2424a0faacc0eb58ca7259853276e0681
Kathryn <kathryn.chu10@gmail.com>
2019-03-13 21:40:57 -0700

    More webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+//   template: './src/index.html',
+//   filename: 'index.html',
+//   inject: 'body'
+// });
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }, 
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
-     ]
- }  
+  }
 };
